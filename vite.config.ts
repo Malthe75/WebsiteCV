719fe2972190c1545e1ea56dbfc77c69e5a132e8
Malthe75 <Malthehf@gmail.com>
2025-12-23 12:29:11 +0100

    [chore] move out of inner folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     tailwindcss(),
     react()
   ], 
-  
-})
+  base: mode === "production" ? "/WebsiteCV/" : "/",
+}));
